5112271fbac79957ef996814da92900babf2aa47
JoelDarnes88 <joeldarnes@gmail.com>
2024-05-03 20:04:17 +0200

    ini

---
+++ demo/src/main/java/com/MatrixCampusSL/demo/BackendMatrixCampusSlApplication.java
@@ -0,0 +1,13 @@
+package com.MatrixCampusSL.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendMatrixCampusSlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendMatrixCampusSlApplication.class, args);
+	}
+
+}
