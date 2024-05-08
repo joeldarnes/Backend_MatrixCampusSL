ecb8c2811b8ad3cba9bbec17ac9640b5a7f3c0d9
JoelDarnes88 <joeldarnes@gmail.com>
2024-05-08 18:48:59 +0200

    V1__Init.sql updated.

--- Backend_MatrixCampusSL/src/main/resources/db/migration/V1__Init.sql
+++ Backend_MatrixCampusSL/src/main/resources/db/migration/V1__Init.sql
@@ -1,15 +1,15 @@
 
-CREATE TABLE prices (
-                        id INT AUTO_INCREMENT PRIMARY KEY,
-                        product_id INT NOT NULL,
-                        brand_id INT NOT NULL,
-                        start_date TIMESTAMP NOT NULL,
-                        end_date TIMESTAMP NOT NULL,
-                        price DECIMAL(10, 2) NOT NULL,
-                        priority INT,
-                        currency VARCHAR(3) NOT NULL,
-                        price_list INT
-);
+CREATE TABLE IF NOT EXISTS prices (
+                                      id INT AUTO_INCREMENT PRIMARY KEY,
+                                      product_id INT NOT NULL,
+                                      brand_id INT NOT NULL,
+                                      start_date TIMESTAMP NOT NULL,
+                                      end_date TIMESTAMP NOT NULL,
+                                      price DECIMAL(10, 2) NOT NULL,
+    priority INT,
+    currency VARCHAR(3) NOT NULL,
+    price_list INT
+    );
 
 INSERT INTO prices (product_id, brand_id, start_date, end_date, price, priority, currency, price_list) VALUES
 (35455, 1, '2020-06-14 00:00:00', '2020-06-14 14:59:59', 35.50, 0, 'EUR', 1),
